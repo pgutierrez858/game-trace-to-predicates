23efd05da5d1ff874a491158ed38944aa7d116ed
Pablo Gutiérrez Sánchez <pabgut02@ucm.es>
2024-06-10 14:52:30 +0100

    Add documentation to ParameterizedPredicateEvaluator.kt

--- src/main/kotlin/predcompiler/compilation/evaluation/evaluators/ParameterizedPredicateEvaluator.kt
+++ src/main/kotlin/predcompiler/compilation/evaluation/evaluators/ParameterizedPredicateEvaluator.kt
@@ -3,28 +3,26 @@ package predcompiler.compilation.evaluation.evaluators
 import predcompiler.compilation.evaluation.RealValuation
 import predcompiler.compilation.evaluation.RewardGenerator
 
+/**
+ * Predicate Evaluator class responsible for evaluating predicates based on various tunable parameters.
+ * Extends the AbstractAutomatonBasedPredicateEvaluator class.
+ *
+ * @property upperLengthBound The upper bound for the length of the predicate.
+ * @property lowerLengthBound The lower bound for the length of the predicate.
+ * @property predicateLengthWeight The weight given to the length of the predicate. The length fitness is scored based on the discrepancy observed between the predicate length and the desired range from the bounds parameters.
+ * @property exampleTraceWasteWeight The weight given to the sample steps not processed by the generated automaton. Waste is computed as the average % of unprocessed steps per trace over trace length.
+ * @property exampleTraceCorrectnessWeight The weight given to the average trace correctness (progress).
+ * @property counterExamplesRejectedWeight The weight given to the NUMBER of counter example traces that are not modelled by the given predicate.
+ */
 class ParameterizedPredicateEvaluator(
     private val upperLengthBound: Int,
     private val lowerLengthBound: Int,
-    /**
-     * Weight given to the length of the predicate. Length fitness is scored based on discrepancy
-     * observed between predicate length and desired range from the bounds parameters.
-     */
     private val predicateLengthWeight: Float,
-    /**
-     * Weight given to the sample steps not processed by the generated automaton. Waste is computed as
-     * the average % of unprocessed steps per trace over trace length.
-     */
     private val exampleTraceWasteWeight: Float,
-    /**
-     * Weight given to the average trace correctness (progress).
-     */
     private val exampleTraceCorrectnessWeight: Float,
-    /**
-     * Weight given to the NUMBER of counter example traces that are not modelled by the given predicate.
-     */
     private val counterExamplesRejectedWeight: Float,
 ) : AbstractAutomatonBasedPredicateEvaluator() {
+
     override fun evaluatePredicate(
         predicate: String, examples: List<Array<RealValuation>>, counterExamples: List<Array<RealValuation>>
     ): Float {
